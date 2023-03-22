697b428e71e27c8b7a0c68ec78744bf57616c81b
Krlitos18 <carlitosmartinezalf.53@gmail.com>
2023-03-21 22:59:02 -0300

    Create Equipo.java

---
+++ src/Equipo.java
@@ -0,0 +1,28 @@
+public class Equipo {
+
+    private String nombre;
+    private String descripcion;  
+
+    //-------------------------------------
+
+    public Equipo(){
+
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getnombre(){
+        return nombre;
+    }
+
+    public String getdescripcion(){
+        return descripcion;
+    }
+    //----------------------------------------
+}
